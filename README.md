4890d2176796cb703f8b851a67ce556215a3ccb6
johnny <johnnyflores@live.ca>
2018-05-14 18:19:47 -0400

    Setting boilerplate for ReactJS with webpack 4

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # react-webpack
 This is a boilerplate for React 16 &amp; Webpack 4.
+
+## Start
+
+``` bash
+# Install dependencies
+npm install
+
+# Serve on localhost:8080
+npm start
+
+# Build for production
+npm run build
+```
+
+## App Info
+
+### Version
+
+1.0.0
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader']
+        })        
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: './src/style.css'
+    })
+  ]
+};
